b9aa37e61fa977c036ae33c364ae91ba1dfb154e
runner150 <kyleettinger@kyles-macbook-pro.local>
2016-07-12 11:37:40 -0500

    Initial Commit

---
+++ Line Graph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Line Graph
+//
+//  Created by Kyle Ettinger on 7/12/16.
+//  Copyright © 2016 Kyle Ettinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
